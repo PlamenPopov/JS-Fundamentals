039cc60cf958fe848b005d35000d7dd63ebe8216
Popov <popov_87@abv.bg>
2019-05-24 14:23:42 +0300

    JS

--- Functions/Tamplate.js
+++ Functions/Tamplate.js
@@ -1,20 +1,22 @@
 function solve(input){
-  let question=input[0];
-  let answer=input[1];
 
-  console.log(
-      `<?xml version="1.0" encoding="UTF-8"?>
-      <quiz>
-      <question>
-      ${question}
-      </question>
-      <answer>
-      ${answer}
-      </answer>
-      </quiz>
-      `
-  );
+  console.log(`<?xml version="1.0" encoding="UTF-8"?>
+  <quiz>`);
+      for (let i = 0; i < input.length; i+=2) {
+            let question=input[i];
+            let answer=input[i+1]; 
+            
+            console.log(
+  ` <question>
+  ${question}
+  </question>
+   <answer>
+    ${answer}
+  </answer>`);
+
+      }
+console.log("</quiz>")
 
 }
 
-solve(["Who?","Me"])
+solve(["Who?","Me","Why?","I do not know!"]);
--- Functions/bakeNumbers.js
+++ Functions/bakeNumbers.js
@@ -1,23 +1,32 @@
 function solve(input){
     
-    let number=input[0];
+    let number=+input[0];
+    
+    let chop=()=>number/2;
+    let dice=()=>Math.sqrt(number);
+    let spice=()=>number+1;
+    let bake=()=>number*3;
+    let fillet=()=>number*0.8;
+
 
     for (let i = 1; i <input.length; i++) {
         if(input[i]=="chop"){
-            number/=2;
+            number=chop();
         }else if(input[i]=="dice"){
-           number=Math.sqrt(number)
+           number=dice()
         }else if(input[i]=="spice"){
-            number+=1;
+            number=spice()
         }else if(input[i]=="bake"){
-            number*=3;
+           number=bake()
         }else if(input[i]=="fillet"){
-            number-=(number/10)*2;
+            number=fillet()
         }
         console.log(number)
     }
 
+   
+
 
 }
 
-solve(['9','dice','spice','chop','bake','fillet'])
+solve(['32','chop','chop','chop','chop','chop'])
