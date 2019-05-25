335197f56a1e760fa6468dc3d382f7af7c90c03a
Popov <popov_87@abv.bg>
2019-05-25 14:34:50 +0300

    JS

--- Functions/radioCristals!!!.js
+++ Functions/radioCristals!!!.js
@@ -2,15 +2,19 @@ function solve(input){
 
 let final=+input[0];
 
-let cut=()=>detail/4;
-let lap=()=>detail*0.8;
-let crind=()=>detail-20;
-let etch=()=>detail-2;
-let xray=()=>detail+1;
-let transport=()=>Math.floor(detail);
+let cut=(x)=>x/4;
+let lap=(x)=>x*0.8;
+let crind=(x)=>x-20;
+let etch=(x)=>x-2;
+let xray=(x)=>x+1;
+let transport=(x)=>parseFloat.Int(x);
+
+input=input.map(Number);
 
 for (let i = 1; i < input.length; i++) {
       detail=input[i]; 
+      console.log(`Processing chunck ${detail} microns`);
+      
 }
 
 }
